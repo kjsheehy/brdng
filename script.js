46dccceac9835669aeaa8f8c8cd944ae57904cd8
kjsheehy <kjsheehy@gmail.com>
2023-02-18 21:13:02 -0500

    rough layout of seat selection page in the passenger UI complete

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const seatMap = document.getElementById('seat-map');
+
+const rows = 20;
+const seats = ['A', 'B', 'C', 'D', 'E'];
+
+for (let i = 0; i < rows; i++) {
+  const newRow = document.createElement('div');
+  newRow.setAttribute('class', 'seat-row');
+  newRow.setAttribute('id', `row${i + 1}`); //Do I ever need this?
+  //console.log(seatMap);
+
+  for (let j = 0; j < seats.length; j++) {
+    let seatButton = document.createElement('button');
+    seatButton.setAttribute('class', 'seat-button');
+    seatButton.setAttribute('id', i + 1 + seats[j]);
+    let seatID = document.createTextNode(i + 1 + seats[j]);
+    seatButton.appendChild(seatID);
+    newRow.appendChild(seatButton);
+  }
+  seatMap.appendChild(newRow);
+}
