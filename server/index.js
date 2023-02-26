d52661e997b8de18fe8b1dcfe9f7831d5a8239a6
kjsheehy <kjsheehy@gmail.com>
2023-02-26 16:07:11 -0500

    added an array for the test set of seats and a function to populate it

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const Joi = require('joi');
-app.use(express.json());
+//const express = require('express');
+//const app = express();
+//const Joi = require('joi');
+//app.use(express.json());
+
+const rows = 20;
+const columns = ['A', 'B', 'C', 'D', 'E'];
+const seats = [];
+
+function populateSeats(rows, columns) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns.length; j++) {
+      let newSeat = {
+        id: i + 1 + columns[j],
+        status: 'Not checked in',
+      };
+      seats.push(newSeat);
+    }
+  }
+}
+
+populateSeats(rows, columns);
