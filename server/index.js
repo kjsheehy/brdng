4b52db3eac5bc708c852d290a8a39d82ae58efa2
kjsheehy <kjsheehy@gmail.com>
2023-02-27 19:24:48 -0500

    Added CREATE Request Handler on API

--- server/index.js
+++ server/index.js
@@ -35,7 +35,38 @@ app.get('/api/seats', (req, res) => {
 });
 
 app.get('/api/seats/:id', (req, res) => {
-  const seat = seats.find((s) => s.id === parseInt(req.params.id));
+  const seat = seats.find((s) => s.id === req.params.id);
   if (!seat) res.status(404).send(`Seat not found with id '${req.params.id}'.`);
   res.send(seat);
 });
+
+//CREATE Request Handler
+// I don't think I'll need this for seats in my application, but I do think I'll need it for something else. I'm going to add this alongside the tutorial I'm following and replace it later on with a CREATE handler that's useful to the application.
+app.post('/api/seats', (req, res) => {
+  const { error } = validateSeats(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  const newSeat = {
+    id: req.body.id,
+    status: req.body.status,
+  };
+  seats.push(newSeat);
+  res.send(newSeat);
+});
+
+//UPDATE Request Handler
+
+function validateSeats(seat) {
+  const schema = Joi.object({
+    id: Joi.string().pattern(new RegExp('[0-9]{1,2}[A-Z]')).required(),
+    status: Joi.string()
+      .valid('Not checked in', 'Checked in', 'Boarding', 'Seated')
+      .required(),
+  });
+  return schema.validate(seat);
+}
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => console.log(`listening on port ${port}...`));
